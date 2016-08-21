de700a8eb9168ef4b8233e1a6ce114ab4095cd91
zhuwei05 <zhuwei5e@qq.com>
2016-08-21 12:30:34 +0800

    chore(demo): add demo

---
+++ py/demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+class TestSolution(unittest.TestCase):
+    def test_demo(self):
+        self.assertEqual(1, 1)
+        self.assertTrue(True)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestSolution)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == '__main__':
+    print 'ok'
+
