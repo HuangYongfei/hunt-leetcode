339ad01bc21253f57327a34f969887f2cb7e5b7c
zhuwei05 <zhuwei5e@qq.com>
2016-08-30 02:12:50 +0800

    feat(p25): Reverse Nodes in k-Group

--- py/p25.py
+++ py/p25.py
@@ -3,50 +3,71 @@
 
 from __future__ import print_function
 
+# 总结： 参考 p24
+# 1. 先取出链表中 k 个节点
+# 2. current 记录 k 个节点的前一个节点
+# 3. 第 k 个节点的 第一个节点（first）的next 指向 第 k+1
+# 4. k 个节点中的 最后一个节点的前一节点是 上 k 个节点的最后一个节点的next
+# 5. 反转这 k 个节点
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
+
 class Solution(object):
-    def swapPairs(self, head):
+    def reverseKGroup(self, head, k):
         """
         :type head: ListNode
+        :type k: int
         :rtype: ListNode
         """
+        # https://discuss.leetcode.com/topic/55969/python-solution-o-n-and-just-76ms
         dummy = ListNode(0)
         dummy.next = head
-        first = dummy.next
-        if not first or not first.next:
-            return dummy.next
+        current = dummy
+        end_node = dummy
+        while True:
+            n = 0
+            while n < k:
+                end_node = end_node.next
+                if not end_node:
+                    return dummy.next
 
-        second = first.next
-        dummy.next = second
-        temp3 = None
-        while first and second:
-            temp1 = None
-            temp2 = None
-            if second and second.next:
-                temp1 = second.next
-            if second.next and second.next.next:
-                temp2 = second.next.next
-            second.next = first
-            first.next = temp1
-            if temp3:
-                temp3.next = second
-            temp3 = first
-            print('first.val %s, second.val %s' % (first.val, second.val))
-            first = temp1
-            second = temp2
-        return dummy.next
+                n += 1
+
+            # 第 k 个节点
+            temp = end_node
+            # 第 k + 1 个节点
+            end_node = end_node.next
+            # k 个节点中的第"一"个节点
+            first = current.next
+            # k 个节点中的第"二"个节点
+            second = current.next.next
+            # 第 k 个节点的 第一个节点（first）的next 指向 第 k+1
+            first.next = end_node
+            # k 个节点中的 最后一个节点的前一节点是 上 k 个节点的最后一个节点的next
+            current.next = temp
+            # 记住该 k 个节点的最后一个节点，作为下 k 个节点的 current
+            next_current_node = first
+            # 逆转 k 个节点
+            for i in range(k - 1):
+                temp = second.next
+                second.next = first
+                first = second
+                second = temp
+
+            current = end_node = next_current_node
+
+
 
     def swapPairs2(self, head):
         dummy = ListNode(0)
         dummy.next = head
         current = dummy
 
-        # 链表反转，取出 k 个元素，反转之
         while current.next and current.next.next:
             first = current.next
             second = current.next.next
