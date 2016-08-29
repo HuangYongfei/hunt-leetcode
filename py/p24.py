315711981d9e46bd4d8365f0b2ae4cb3e4a2644f
zhuwei05 <zhuwei5e@qq.com>
2016-08-29 10:44:57 +0800

    refac(p24): Swap Nodes in Pairs

--- py/p24.py
+++ py/p24.py
@@ -40,6 +40,22 @@ class Solution(object):
             second = temp2
         return dummy.next
 
+    def swapPairs2(self, head):
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+
+        while current.next and current.next.next:
+            first = current.next
+            second = current.next.next
+
+            current.next = second
+            first.next = second.next
+            second.next = first
+
+            current = current.next.next
+        return dummy.next
+
 
 
 def gen_list(arr):
