b2e9fe4f3310bac11988220eedd98a2f028c7655
zhuwei05 <zhuwei5e@qq.com>
2016-09-18 08:46:40 +0800

    refac(p69): add solution

--- py/p69.py
+++ py/p69.py
@@ -7,6 +7,23 @@
 # 总结：
 #
 
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) / 2
+            if mid ** 2 <= x < (mid + 1) ** 2:
+                return mid
+            if mid ** 2 > x:
+                right = mid - 1s
+            else:
+                left = mid + 1
+
+
 
 
 import unittest
