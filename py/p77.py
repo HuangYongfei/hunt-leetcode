3cdd405369a8b958687927db8ce2dca09aa7d138
zhuwei05 <zhuwei5e@qq.com>
2016-09-19 14:55:22 +0800

    refac(p77): opt backtrace

--- py/p77.py
+++ py/p77.py
@@ -17,7 +17,7 @@ class Solution(object):
         """
         path = []
         res = []
-        self.backtrace(n, k, 1, path, res)
+        self.backtrace2(n, k, 1, path, res)
         return res
 
     def backtrace(self, n, k, start, path, res):
@@ -39,13 +39,29 @@ class Solution(object):
             res.append(path[:])
             return
 
+        for i in range(start, n+1):
+            # You can stop much earlier if you notice that number of remaining elements is smaller than needed to fill combination
+            if n - start + 1 < k - len(path):
+                return
+            path.append(i)
+            self.backtrace2(n, k, i+1, path, res)
+            path.pop()
+
+    def backtrace3(self, n, k, start, path, res):
+        if 0 == k:
+            # 使用切片复制数组，否则只是path的引用，会随path的改变而改变
+            res.append(path[:])
+            return
+
         for i in range(start, n-k+1+1):
             path.append(i)
-            self.backtrace(n, k, i+1, path, res)
+            self.backtrace3(n, k-1, i+1, path, res)
             path.pop()
 
     # itertools
+
     def combine2(self, n, k):
+        import itertools
         return [each for each in itertools.combinations(range(1, n + 1), k)]
 
 import unittest
@@ -59,6 +75,7 @@ unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == '__main__':
     print Solution().combine(4, 2)
+    # print Solution().combine2(4, 2)
     # print Solution().combine(20, 16)
     print 'ok'
 
