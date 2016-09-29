35b29bc932c62d64291340c79631c760200789da
zhuwei05 <zhuwei5e@qq.com>
2016-09-29 11:37:07 +0800

    refac(p86): straightforward

--- py/p86.py
+++ py/p86.py
@@ -45,6 +45,36 @@ class Solution(object):
         first_curr.next = second.next
         return first.next
 
+    # 1. 找出开头处相连的小于x的(t_list)最后一个节点t
+    # 2. 从t.next（p）开始，找出接下来相连的大于等于x的（list_p)最后一个节点p
+    # 3. 将t和p.next连接，同时p.next指向list_p的开头，list_p的结尾指向p.next.next
+    # 4. 循环2，3
+    def partition2(self, head, x):
+        dummy = ListNode(None)
+        dummy.next = head
+        t = dummy
+        # 1.
+        while t.next and t.next.val < x:
+            t = t.next
+        # 此时t指向开头一串节点中小于x的最后一个节点，p则为>=x的第一个节点或为末尾节点
+        p = t.next
+        if not p:
+            return dummy.next
+
+        # 4.
+        while p.next:
+            # 2.
+            while p.next and p.next.val >= x:
+                p = p.next
+            # 3.
+            if p.next:
+                tmp = p.next
+                p.next = p.next.next
+                tmp.next = t.next
+                t.next = tmp
+                t = tmp
+
+        return dummy.next
 
 def gen_list(arr):
     head = None
@@ -76,14 +106,21 @@ suite = unittest.TestLoader().loadTestsFromTestCase(TestSolution)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == '__main__':
-    disp_list(Solution().partition(gen_list([1, 4, 3, 2, 5, 2]), 3))
-    disp_list(Solution().partition(gen_list([1]), 2))
-    disp_list(Solution().partition(gen_list([2, 1]), 2))
-    disp_list(Solution().partition(gen_list([1, 4, 3, 4, 5]), 2))
-    disp_list(Solution().partition(gen_list([3, 4, 2, 2, 1]), 5))
-    disp_list(Solution().partition(gen_list([3, 4, 2, 2, 1]), 2))
+    # disp_list(Solution().partition(gen_list([1, 4, 3, 2, 5, 2]), 3))
+    # disp_list(Solution().partition(gen_list([1]), 2))
+    # disp_list(Solution().partition(gen_list([2, 1]), 2))
+    # disp_list(Solution().partition(gen_list([1, 4, 3, 4, 5]), 2))
+    # disp_list(Solution().partition(gen_list([3, 4, 2, 2, 1]), 5))
+    # disp_list(Solution().partition(gen_list([3, 4, 2, 2, 1]), 2))
+    # disp_list(Solution().partition(gen_list([]), 5))
 
-    disp_list(Solution().partition(gen_list([]), 5))
+    disp_list(Solution().partition2(gen_list([1, 4, 3, 2, 5, 2]), 3))
+    disp_list(Solution().partition2(gen_list([1]), 2))
+    disp_list(Solution().partition2(gen_list([2, 1]), 2))
+    disp_list(Solution().partition2(gen_list([1, 4, 3, 4, 5]), 2))
+    disp_list(Solution().partition2(gen_list([3, 4, 2, 2, 1]), 5))
+    disp_list(Solution().partition2(gen_list([3, 4, 2, 2, 1]), 2))
+    disp_list(Solution().partition2(gen_list([]), 5))
 
 
     print('ok')
