903486347c03f3411ff9973dc551458b633b47ba
zhuwei05 <zhuwei5e@qq.com>
2016-10-06 20:07:29 +0800

    feat(p13): Roman to Integer

---
+++ py/p13.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+
+# 13. Roman to Integer
+# Given a roman numeral, convert it to an integer.
+# Input is guaranteed to be within the range from 1 to 3999.
+
+
+# 总结：
+#
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+        z = 0
+        for i in range(0, len(s) - 1):
+            if roman[s[i]] < roman[s[i+1]]:
+                z -= roman[s[i]]
+            else:
+                z += roman[s[i]]
+        return z + roman[s[-1]]
+
+
+
+import unittest
+class TestSolution(unittest.TestCase):
+    def test_demo(self):
+        self.assertEqual(1, 1)
+        self.assertTrue(True)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestSolution)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == '__main__':
+    print('ok')
+
