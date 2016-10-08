d92934cba253d93f908742705542ea59fde9c38d
zhuwei05 <zhuwei5e@qq.com>
2016-10-08 22:07:58 +0800

    refac(p148): use quicksort

--- py/p148.py
+++ py/p148.py
@@ -46,6 +46,47 @@ class Solution(object):
         p.next = p.next = l1 or l2
         return dummy.next
 
+    # https://discuss.leetcode.com/topic/39467/python-quick-sort-solution-easy-to-understand-beats-95-51
+    def sortList2(self, head):
+        new_head = ListNode(None)
+        new_head.next = head
+        self.quicksort(new_head, None)
+        return new_head.next
+
+    @staticmethod
+    def quicksort(start, end):
+        if start.next != end:
+            prev, post = Solution.partition(start, end)
+            Solution.quicksort(start, prev)
+            Solution.quicksort(post, end)
+
+    @staticmethod
+    def partition(start, end):
+        #  We just pick the first element as pivot. And if we encounter a node with the same value as pivot, we move the pivotPost one step forward. When partition function returns, the pivot becomes the end node for the former half while the pivotPost becomes the start node for the latter half. In this way, we don't have to sort nodes between two pivots in the next round.
+        node = start.next.next
+        # 取第一个节点作为pivot
+        pivot = start.next
+        pivot.next = end
+        pivotPost = pivot
+        while node != end:
+            temp = node.next
+            if node.val > pivot.val:
+                # 在post头部后插入大于当前节点的节点
+                node.next = pivotPost.next
+                pivotPost.next = node
+            elif node.val < pivot.val:
+                # 在start头部后插入下雨当前节点的节点
+                node.next = start.next
+                start.next = node
+            else:
+                # 相等时，同样在post头部后插入当前节点，并移动post
+                node.next = pivotPost.next
+                pivotPost.next = node
+                pivotPost = pivotPost.next
+            node = temp
+        return [pivot, pivotPost]
+
+
 def gen_list(arr):
     head = None
     prev = None
