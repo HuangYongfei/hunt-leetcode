6bfb296e1838f1a9199c0c6f1b4222c30687b296
zhuwei05 <zhuwei5e@qq.com>
2016-10-08 14:27:42 +0800

    refac(p21): use recursive

--- py/p21.py
+++ py/p21.py
@@ -67,6 +67,18 @@ class Solution(object):
         tail.next = l1 or l2
         return dummy.next
 
+    def mergeTwoListsRecur(self, l1, l2):
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoListsRecur(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoListsRecur(l1, l2.next)
+            return l2
 
 def gen_list(arr):
     prev = head = None
