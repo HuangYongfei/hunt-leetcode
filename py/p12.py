0b8de241f6efbff3bf67307d747a03cc52c7de62
zhuwei05 <zhuwei5e@qq.com>
2016-10-08 08:53:57 +0800

    feat(p12): Integer to Roman

--- py/p12.py
+++ py/p12.py
@@ -3,9 +3,27 @@
 
 from __future__ import print_function, unicode_literals, absolute_import
 
+# 12. Integer to Roman
+# Given an integer, convert it to a roman numeral.
+# Input is guaranteed to be within the range from 1 to 3999.
+
+
 # 总结：
 #
 
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        dict = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        result = ""
+        for letter, n in zip(dict, nums):
+            result += letter * int(num / n)
+            num %= n
+        return result
 
 
 import unittest
