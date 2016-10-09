c45ef400710f19ebc6ce64bf27be5fc7b5855776
zhuwei05 <zhuwei5e@qq.com>
2016-10-09 08:40:51 +0800

    feat(p237): Delete Node in a Linked List, replace the val of deleted node with its next node val

---
+++ py/p237.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+# 237. Delete Node in a Linked List
+# Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        p = node.next
+        if p:
+            node.val = p.val
+            node.next = p.next
+
+
+def gen_list(arr):
+    head = None
+    prev = None
+    for x in arr:
+        node = ListNode(x)
+        if not head:
+            prev = head = node
+        else:
+            prev.next = node
+            prev = prev.next
+    return head
+
+
+def disp_list(l):
+    while(l):
+        s = "%s -> " % l.val
+        print(s, end='')
+        l = l.next
+    print('\n')
+
+import unittest
+class TestSolution(unittest.TestCase):
+    def test_demo(self):
+        self.assertEqual(1, 1)
+        self.assertTrue(True)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestSolution)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == '__main__':
+    print('ok')
+
