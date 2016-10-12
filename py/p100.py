270df4431ed0c6c8d032d86f7e95adc068c00be1
zhuwei05 <zhuwei5e@qq.com>
2016-10-12 13:07:31 +0800

    feat(p100): Same Tree, use recursion to handle subtree

---
+++ py/p100.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+
+#
+
+
+# 总结：
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+import unittest
+class TestSolution(unittest.TestCase):
+    def test_demo(self):
+        self.assertEqual(1, 1)
+        self.assertTrue(True)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestSolution)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == '__main__':
+    print('ok')
+
